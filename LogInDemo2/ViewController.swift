93fbeeec16fb97168ffd8a06492b2c8f1bd02bc7
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-21 16:35:48 +0300

    Initial Commit

---
+++ LogInDemo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInDemo2
+//
+//  Created by ALEKSEY SUSLOV on 21.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
