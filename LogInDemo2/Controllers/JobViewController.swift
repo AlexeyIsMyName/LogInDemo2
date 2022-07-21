e1140af422b5ad3684cce0aae8c268a07b491a3a
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-21 17:42:34 +0300

    Added files for Custom Controllers

---
+++ LogInDemo2/Controllers/JobViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JobViewController.swift
+//  LogInDemo2
+//
+//  Created by ALEKSEY SUSLOV on 21.07.2022.
+//
+
+import UIKit
+
+class JobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
